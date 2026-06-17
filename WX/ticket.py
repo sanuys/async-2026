261e41305efcac18d92d7d2893f1e7bd99a27635
sanuys <sanuseit@gmail.com>
2026-06-17 10:06:43 +0700

    fix ticket

--- WX/ticket.py
+++ WX/ticket.py
@@ -2,7 +2,14 @@ import sys
 
 def calculate_ticket_price(age):
     # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
-    pass
+    if age <= 0:
+        return "Invalid age"
+    elif age <= 12:
+        return 100
+    elif age <= 60:
+        return 200
+    else:
+        return 150
     # --------------------------------------------------------
 
 def main():
@@ -12,7 +19,7 @@ def main():
         result = calculate_ticket_price(test_age)
         print(result)
     else:
-        test_age = 25
+        test_age = 61
         result = calculate_ticket_price(test_age)
         print(f"Age: {test_age} -> Ticket Price: {result} Baht")
 
