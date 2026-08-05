42eb5b69a525c2105dab37bedae27d7e70285738
sanuys <sanuseit@gmail.com>
2026-08-05 11:36:12 +0700

    ืnot done แต่ส่งไปก่อนเดี๋ยวส่งไม่ทัน

--- Week6/robots.py
+++ Week6/robots.py
@@ -5,8 +5,8 @@ import httpx
 # ==========================================
 # 1. Configuration & Constants
 # ==========================================
-STUDENT_ID = "" 
-BASE_URL = ""
+STUDENT_ID = "6710301025" 
+BASE_URL = "http://172.16.2.117:8088/{student_id}/monitor"
 
 # กำหนดลำดับชิ้นส่วนและหุ่นยนต์
 PARTS = ["A", "B", "C"]
@@ -19,17 +19,27 @@ ROBOTS = ["robot_1", "robot_2", "robot_3"]
 async def reset_factory(client: httpx.AsyncClient):
     """ส่ง Request เพื่อทำการ Reset สถานะของหุ่นยนต์ทั้งหมดของรหัสนักเรียนนี้"""
     # TODO: เติมโค้ดการส่ง POST request ไปยัง /student/{STUDENT_ID}/reset
+    url = f"{BASE_URL}/reset"
+    response = await client.post(url)
+    return response.json()
     pass
 
 async def grab_part(client: httpx.AsyncClient, robot_id: str, part: str):
     """สั่งให้หุ่นยนต์หยิบชิ้นส่วน 1 ชิ้น"""
     # TODO: เติมโค้ดส่ง POST request ไปยัง /student/{STUDENT_ID}/robot/{robot_id}/grab
     # พร้อมแนบ JSON Payload {"part": part}
+    url = f"{BASE_URL}/robot/{robot_id}/grab"
+    response = await client.post(url, json={"part": part})
+    return response.json()
     pass
 
 async def run_robot_task(client: httpx.AsyncClient, robot_id: str):
     """สั่งให้หุ่นยนต์ 1 ตัว ทำการหยิบชิ้นส่วน A, B, และ C ตามลำดับ"""
     # TODO: วนลูปหยิบชิ้นส่วนใน PARTS ตามลำดับเรียงกัน (Sequential inside single robot)
+    for part in PARTS:
+        await grab_part(client, robot_id, part)
+        grap_response = await client.get(f"{BASE_URL}/robot/{robot_id}/status")
+        return grap_response.json()
     pass
 
 async def main():
@@ -42,7 +52,12 @@ async def main():
         print("Starting Async Robot Operation...")
         
         # TODO: สั่งรัน run_robot_task ของหุ่นยนต์ทั้ง 4 ตัวพร้อมกันโดยใช้ asyncio.gather
-        
+        await asyncio.gather(
+            run_robot_task(client, "robot_1"),
+            run_robot_task(client, "robot_2"),
+            run_robot_task(client, "robot_3")
+        )
+
         elapsed_time = time.time() - start_time
         print(f"Finished all tasks in {elapsed_time:.2f} seconds.")
 
